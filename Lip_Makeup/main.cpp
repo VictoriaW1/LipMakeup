3e90dea8fae198491a027969a664b535b3e3b83a
Victoria <wyvictoria1@gmail.com>
2017-03-30 16:42:03 +0800

    Image force with RGB

--- Lip_Makeup/main.cpp
+++ Lip_Makeup/main.cpp
@@ -15,22 +15,28 @@ using namespace cv;
 
 String IMG_DIR  = "D:\\visual studio 2015\\Projects\\Lip_Makeup\\images\\";
 String IMG_PATH ;
-String IMG_FN = "objs1";
+String IMG_FN = "mouth_men2";
+String EXTENSION = ".jpg";
 String VIDEO_DIR = "D:\\visual studio 2015\\Projects\\Lip_Makeup\\videos\\";
 String VIDEO_PATH;
 const int MAX_ITER = 500;
 const float TIME_STEP = 10;
 const float MU = 0.5;
 const float V = 0.0;
-const float LAMBDA1 = 1;
-const float LAMBDA2 = 1;
+const float R_LAMBDA1 = 1;
+const float R_LAMBDA2 = 1;
+const float G_LAMBDA1 = 0.5;
+const float G_LAMBDA2 = 0.5;
+const float B_LAMBDA1 = 0.8;
+const float B_LAMBDA2 = 0.8;
 const float LAMBDA = 0.8;
 const double EPSILON = 1;
 const float X_CENTER = 100;
 const float Y_CENTER = 100;
 const float RADIUS = 100;
-const int MAX_LENGTH_OF_IMAGE = 1000000;
+const int MAX_LENGTH_OF_IMAGE = 10000000;
 const float BAND = 2;
+
 
 int splitIntoFrames(string file_path) {
 	CvCapture *capture = cvCaptureFromFile(file_path.c_str());
@@ -54,6 +60,80 @@ void mergeIntoVideo(string imgs_path, string video_fn) {
 
 	
 }
+
+/*
+Detect lip region based on RGB color space
+*/
+void RGBDetectLipRegion(string file_path) {
+	Mat img = imread(file_path);
+	int rows = img.rows;
+	int cols = img.cols;
+	uchar* p;
+	int b, g, r;
+	vector<float> YIQ;
+	for (int i = 0; i < rows; ++i) {
+		p = img.ptr<uchar>(i);
+		for (int j = 0; j < cols; ++j) {
+			b = p[j * 3];
+			g = p[j * 3 + 1];
+			r = p[j * 3 + 2];
+			//cout << "bgr" << b << g << r << endl;
+			
+			if (log10(g / (pow(b, 0.391) * pow(r, 0.609))) < -0.15) {
+				p[j * 3] = 0;
+				p[j * 3 + 1] = 255;
+				p[j * 3 + 2] = 0;
+			}
+		}
+	}
+	namedWindow("RGB Detect Lip Region");
+	imshow("RGB Detect Lip Region", img);
+	waitKey(50000);
+	imwrite(IMG_DIR + IMG_FN + "_RGBRegion.jpg", img);
+}
+/*
+Detect lip region based on YIQ statistic
+*/
+vector<float> RGB2YIQ(float R, float G, float B) {
+	float Y = 0.299 * R + 0.587 * G + 0.114 * B;
+	float I = 0.596 * R - 0.275 * G - 0.321 * B;
+	float Q = 0.212 * R - 0.523 * G + 0.311 * B;
+	vector<float> YIQ;
+	YIQ.push_back(Y);
+	YIQ.push_back(I);
+	YIQ.push_back(Q);
+	return YIQ;
+}
+
+void YIQDetectLipRegion(string file_path) {
+	Mat img = imread(file_path, 1);//BGR
+	int rows = img.rows;
+	int cols = img.cols;
+	uchar* p;
+	int b, g, r;
+	vector<float> YIQ;
+	for (int i = 0; i < rows; ++i) {
+		p = img.ptr<uchar>(i);
+		for (int j = 0; j < cols; ++j) {
+			b = p[j * 3];
+			g = p[j * 3 + 1];
+			r = p[j * 3 + 2];
+			//cout << "bgr" << b << g << r << endl;
+			YIQ = RGB2YIQ(r, g, b);
+			if (YIQ[0] >= 80 && YIQ[0] <= 220 && YIQ[1] >= 12 && YIQ[1] <= 78 && YIQ[2] >= 7 && YIQ[2] <= 25) {
+				p[j * 3] = 255;
+				p[j * 3 + 1] = 0;
+				p[j * 3 + 2] = 255;
+			}
+		}
+	}
+	namedWindow("YIQ Detect Lip Region");
+	imshow("YIQ Detect Lip Region", img);
+	waitKey(50000);
+	imwrite(IMG_DIR + IMG_FN + "_YIQRegion.jpg", img);
+}
+
+
 /*
 Algorithm in Active Contour Without Edge.
 */
@@ -129,7 +209,8 @@ void neumannBoundCond(Mat f) {
 	g.col(cols - 1) = g.col(cols - 3);
 	f = g;
 }
-void ACWE(Mat img, Mat phi, int max_iter, float time_step, float mu, float v, float lambda1, float lambda2, float lambda, double epsilon) {
+void ACWE(Mat color_img, Mat img, Mat phi, int max_iter, float time_step, float mu, float v, float r_lambda1, float r_lambda2, 
+	float g_lambda1, float g_lambda2, float b_lambda1, float b_lambda2, float lambda, double epsilon) {
 	Mat original_img;
 	img.convertTo(original_img, CV_8UC1);
 	//Mat phi = phi0;
@@ -170,14 +251,23 @@ void ACWE(Mat img, Mat phi, int max_iter, float time_step, float mu, float v, fl
 		neumannBoundCond(phi);
 		cout << "ACWE iter: " << itr << endl;
 
-		float c1 = 0;
-		float c2 = 0;
+		float cr1 = 0;
+		float cr2 = 0;
+		float cg1 = 0;
+		float cg2 = 0;
+		float cb1 = 0;
+		float cb2 = 0;
 		int neighbor_num = 0;
 		int inside_num = 0;
 		int outside_num = 0;
 
+		uchar* p;
 		for (int i = 0; i < rows; ++i) {
+			p = color_img.ptr<uchar>(i);
 			for (int j = 0; j < cols; ++j) {
+				int b = p[3 * j];
+				int g = p[3 * j + 1];
+				int r = p[3 * j + 2];
 				//Neigbor of curve phi
 				if (phi.at<float>(i,j) < BAND & phi.at<float>(i, j) > -BAND) {
 					*(x_of_curve_neighbor + neighbor_num) = j;
@@ -187,14 +277,20 @@ void ACWE(Mat img, Mat phi, int max_iter, float time_step, float mu, float v, fl
 
 				//Mean value inside curve of img
 				if (0 <= phi.at<float>(i, j)) {
-					c1 += img.at<float>(i, j);
+					//c1 += img.at<float>(i, j)
+					cr1 += r;
+					cg1 += g;
+					cb1 += b;
 					*(x_inside + inside_num) = j;
 					*(y_inside + inside_num) = i;
 					++inside_num;
 				}
 				//Mean value outside curve of img
 				if (phi.at<float>(i, j) < 0) {
-					c2 += img.at<float>(i, j);
+					cr2 += r;
+					cg2 += g;
+					cb2 += b;
+					//c2 += img.at<float>(i, j);
 					*(x_outside + outside_num) = j;
 					*(y_outside + outside_num) = i;
 					++outside_num;
@@ -211,9 +307,23 @@ void ACWE(Mat img, Mat phi, int max_iter, float time_step, float mu, float v, fl
 			internal_force = curvatureCentral(phi);
 
 			//Image force
-			c1 /= (inside_num + 0.00000001);
-			c2 /= (outside_num + 0.000000001);
-			image_force = -lambda1 * (img - c1).mul(img - c1) + lambda2 * (img - c2).mul(img - c2);
+			//c1 /= (inside_num + 0.00000001);
+			//c2 /= (outside_num + 0.000000001);
+			cr1 /= (inside_num + 0.00000001);
+			cr2 /= (outside_num + 0.000000001);
+			cg1 /= (inside_num + 0.00000001);
+			cg2 /= (outside_num + 0.000000001);
+			cb1 /= (inside_num + 0.00000001);
+			cb2 /= (outside_num + 0.000000001);
+			//image_force = -lambda1 * (img - c1).mul(img - c1) + lambda2 * (img - c2).mul(img - c2);
+			Mat channels[3];
+			split(color_img, channels);
+			image_force = -( b_lambda1 * (channels[0] - cb1).mul(channels[0] - cb1)
+				+ g_lambda1 * (channels[1] - cg1).mul(channels[1] - cg1)
+				+ r_lambda1 * (channels[2] - cr1).mul(channels[2] - cr1)) / 3
+				+ (b_lambda2 * (channels[0] - cb2).mul(channels[0] - cb2)
+					+ g_lambda2 * (channels[1] - cg2).mul(channels[1] - cg2)
+					+ r_lambda2 * (channels[2] - cr2).mul(channels[2] - cr2)) / 3;
 			double* max_image_force = new double;
 			minMaxIdx(image_force, NULL, max_image_force);
 			//cout << "max image_force: " << lambda * *max_image_force << endl;
@@ -261,7 +371,7 @@ void ACWE(Mat img, Mat phi, int max_iter, float time_step, float mu, float v, fl
 			findContours(phi_8UC1, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 			img.convertTo(original_img, CV_8UC1);
 			drawContours(original_img, contours, -1, CV_RGB(255, 0, 0));
-			imwrite((IMG_DIR + IMG_FN + "_contour.jpg").c_str(), original_img);
+			imwrite((IMG_DIR + IMG_FN + "_rgbcontour.jpg").c_str(), original_img);
 			cout << "contour size: " << contours.size() << endl;
 			//imshow(window_name, original_img);
 			//cvWaitKey(0.1);
@@ -332,15 +442,17 @@ void detectLipContInVideo(string file_path, bool show_contour = true) {
 		cout << "rows:" << img.rows << endl;
 		cout << "cols: " << img.cols << endl;
 		Mat phi0 = initializePhi(img.rows, img.cols, X_CENTER, Y_CENTER, RADIUS);
-		ACWE(img0, phi0, MAX_ITER, TIME_STEP, MU, V, LAMBDA1, LAMBDA2, LAMBDA, EPSILON);	
+		//ACWE(img0, phi0, MAX_ITER, TIME_STEP, MU, V, LAMBDA1, LAMBDA2, LAMBDA, EPSILON);	
 	}
 
 }
 
 void detectLipContInImage(string img_path, bool show_contour = true) {
-	VIDEO_PATH = VIDEO_DIR + IMG_FN + "_phi.avi";
+	VIDEO_PATH = VIDEO_DIR + IMG_FN + "_rgbphi.avi";
 	cout << "video path:" << VIDEO_PATH << endl;
 	Mat img = imread(img_path, CV_LOAD_IMAGE_GRAYSCALE);
+	Mat color_img = imread(img_path, CV_LOAD_IMAGE_COLOR);
+	color_img.convertTo(color_img, CV_32F);
 	Mat img0;
 	img.convertTo(img0, CV_32F);
 	/*/
@@ -352,7 +464,7 @@ void detectLipContInImage(string img_path, bool show_contour = true) {
 	cout << "rows:" << img.rows << endl;
 	cout << "cols: " << img.cols << endl;
 	Mat phi0 = initializePhi(img.rows, img.cols, X_CENTER, Y_CENTER, RADIUS);
-	ACWE(img0, phi0, MAX_ITER, TIME_STEP, MU, V, LAMBDA1, LAMBDA2, LAMBDA, EPSILON);
+	ACWE(color_img, img0, phi0, MAX_ITER, TIME_STEP, MU, V, R_LAMBDA1, R_LAMBDA2, G_LAMBDA1, G_LAMBDA2, B_LAMBDA1, B_LAMBDA2, LAMBDA, EPSILON);
 	cout << "jijiiji" << endl;
 
 
@@ -361,5 +473,7 @@ int main() {
 	cout << "split video into frames" << endl;
 	string video_path("F:\\Project\\LipMakeup\\video\\video.mp4");
 	//detectLipContInVideo(video_path);
-	detectLipContInImage(IMG_DIR + IMG_FN + ".png");
+	//YIQDetectLipRegion(IMG_DIR + IMG_FN + EXTENSION);
+	RGBDetectLipRegion(IMG_DIR + IMG_FN + EXTENSION);
+	//detectLipContInImage(IMG_DIR + IMG_FN + EXTENSION);
 }
